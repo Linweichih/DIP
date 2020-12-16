498e41ec59145523c51d71c7e738d8debaf668b3
Linweichih <43320046+Linweichih@users.noreply.github.com>
2020-12-16 22:29:30 +0800

    pro5

--- Project5/project5.m
+++ Project5/project5.m
@@ -9,35 +9,44 @@ Im = imread('Car On Mountain Road.tif');
 Im = im2double(Im);
 
 %% Figures of the LoG image 
-LoG_kernal = fspecial('log', 25, 4);
-LoG_Im = imfilter(Im, LoG_kernal);
-% LoG_Im = mat2gray(LoG_Im);
-figure('Name','Figures of the LoG image','NumberTitle','off');
+n = 7;
+sigma = 4;
+LoG_kernal = fspecial('log', n,sigma);
+LoG_Im = filter2(LoG_kernal,Im);
+
+figure('Name','Figures of the LoG image ','NumberTitle','off');
 imshow(mat2gray(LoG_Im));
-%% two thresholds: 0% and 4% the maximum gray level of the LoG image 
+
+%% two thresholds: 0% and 4% the maximum gray level of the LoG image
 max_log = max(LoG_Im,[],'all');
-LoG_0_Im = zeros(size(Im));
-LoG_0_Im(LoG_Im > 0) = 1;
-LoG_4_Im = zeros(size(Im));
-LoG_4_Im(LoG_Im > (0.04*max_log)) = 1;
-BW_edge = edge(LoG_4_Im,'Canny');
+LoG_0_Im = edge(LoG_Im,'zerocross',0);
+LoG_4_Im = edge(LoG_Im,'zerocross',0.04*max_log);
+figure('Name','0% the maximum gray level of the LoG image','NumberTitle','off');
+imshow(LoG_0_Im);
+figure('Name','4% the maximum gray level of the LoG image','NumberTitle','off');
+imshow(LoG_4_Im);
+
 %% Figure of Hough parameter space 
-[H,Theta,Rho] = hough(BW_edge);
+[H,Theta,Rho] = hough(LoG_4_Im,'Theta',-90:1:89);
 figure
 imshow(H,[],'XData',Theta,'YData',Rho,'InitialMagnification','fit');
 xlabel('\theta')
 ylabel('\rho');
 axis on, axis normal,hold on;
 %% Figures of linked edges alone and overlapped on the original image
-P  = houghpeaks(H,2);
+P = houghpeaks(H,2000,'Threshold',0.00001*max(H(:)));
 x = Theta(P(:,2)); 
 y = Rho(P(:,1));
-plot(x,y,'s','color','r');
-% lines = houghlines(BW_edge,Theta,Rho,P,'MinLength',10);
-% figure, imshow(LoG_4_Im), hold on
-% max_len = 0;
-% for k = 1:length(lines)
-%  xy = [lines(k).point1; lines(k).point2];
-%  plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','r');
-% end
+% plot(x,y,'s','color','r');
+lines = houghlines(LoG_4_Im,Theta,Rho,P,'FillGap',3,'MinLength',6);
+figure,imshow(LoG_Im), hold on
+for k = 1:length(lines)
+ xy = [lines(k).point1; lines(k).point2];
+ plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','r');
+end
+figure, imshow(Im), hold on
+for k = 1:length(lines)
+ xy = [lines(k).point1; lines(k).point2];
+ plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','r');
+end
 
